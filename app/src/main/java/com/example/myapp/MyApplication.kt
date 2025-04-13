d69a31a379bd1e13d98a984dba4b3d8c94773618
oumaima <aitchekhoumaima25@gmail.com>
2025-04-13 13:57:25 +0100

    Initialize MyApp with basic structure, resources, and configurations

---
+++ app/src/main/java/com/example/myapp/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.myapp
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialisation de Firebase
+        FirebaseApp.initializeApp(this)
+    }
+
+}
